45c03a161cc1cbdeabdb86b0674acf9cf3cec88c
Code Zero Null <code0@runbox.com>
2024-09-13 13:18:29 +0200

    Started with Fibonacci generator

--- mtools.py
+++ mtools.py
@@ -23,3 +23,14 @@ def sd(input_list):
     s = sum([(member - mean) ** 2 for member in input_list])
     n = len(input_list)
     return (s / (n - 1)) ** 0.5
+
+
+def fibo(input, optional):
+    """
+    Generate Fibonacci sequence
+    Input: Lenght of numbers to output
+    Optional Input: Starting point:
+     - 0: 0 and 1 (default)
+     - 1: 1 and 1
+     - 2: 1 and 2 (Fibonacci's way)
+    """
